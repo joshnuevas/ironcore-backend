841f51ca4e75d31802afa45e279e73e3d94539aa
MystoganF <159971401+MystoganF@users.noreply.github.com>
2025-11-02 13:30:02 +0800

    updated readme

--- ironcore-backend/src/main/java/com/ironcore/ironcorebackend/controller/AuthController.java
+++ ironcore-backend/src/main/java/com/ironcore/ironcorebackend/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000") // allow frontend
+@CrossOrigin(origins = "http://localhost:3000") // allow frontend 
 public class AuthController {
 
     @Autowired
